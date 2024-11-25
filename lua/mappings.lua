3df3ac516f60b2f60d08c62470304c8ea916fada
Arinji2 <arinjaydhar205@gmail.com>
2024-11-25 15:28:35 +0530

    Add easy shortcuts to find files, and live grep

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,3 +3,5 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "ff", "<cmd>Telescope find_files<CR>")
+map("n", "fg", "<cmd>Telescope live_grep<CR>")
