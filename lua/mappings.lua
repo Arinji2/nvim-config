205c7c26e8975e6fa4eb2316732222fbb3c482a5
Arinji2 <arinjaydhar205@gmail.com>
2024-12-14 00:54:08 +0530

    Remove scuffed telescope shortcuts
    Use the existing ones :/

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "ff", "<cmd>Telescope find_files<CR>")
-map("n", "fg", "<cmd>Telescope live_grep<CR>")
 map("i", "<C-s>", "<Esc>:w<CR>i", { noremap = true, silent = true })
 
 local Terminal = require("toggleterm.terminal").Terminal
