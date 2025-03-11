4e67212075944b69589a3305a42e661160b5be6e
Arinji2 <arinjaydhar205@gmail.com>
2025-03-11 19:27:02 +0530

    Disable Auto Session for Homedir

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -4,7 +4,6 @@ return {
   ---@module "auto-session"
   opts = {
     suppressed_dirs = { "~/Projects", "~/Downloads" },
-    auto_restore_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
   },
   config = function(_, opts)
     require("auto-session").setup(opts)
