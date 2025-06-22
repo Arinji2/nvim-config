f3048e15c3f823f4e849d9011dc892cdc889e6ef
Arinji2 <arinjaydhar205@gmail.com>
2025-06-22 23:10:28 +0530

    Update to 0.11 lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,29 +1,12 @@
 -- Load defaults (e.g., lua_lsp)
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- List of LSP servers with default configurations
 local servers = require("languages").lsp_servers
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- Setup LSP servers with default configs
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
 
 -- GO language server
-lspconfig.gopls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+vim.lsp.config("gopls", {
+  -- filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  -- root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
     gopls = {
       analyses = {
@@ -32,34 +15,11 @@ lspconfig.gopls.setup {
       staticcheck = true,
     },
   },
-}
+})
 
-local jsonCapabilities = vim.lsp.protocol.make_client_capabilities()
-jsonCapabilities.textDocument.completion.completionItem.snippetSupport = true
--- JSON language server
-lspconfig.jsonls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = jsonCapabilities,
-  cmd = { "vscode-json-language-server", "--stdio" },
-  filetypes = { "json" },
-}
-
-lspconfig.tailwindcss.setup {
-  on_attach = function(client, bufnr)
-    -- Disable semantic tokens to prevent Tailwind from taking priority
-    client.server_capabilities.semanticTokensProvider = nil
-
-    -- Call your existing on_attach function if needed
-    if nvlsp.on_attach then
-      nvlsp.on_attach(client, bufnr)
-    end
-  end,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
+vim.lsp.config("tailwindcss", {
   settings = {
     tailwindCSS = {
-
       classAttributes = { "class", "className", "classList" },
       emmetCompletions = true,
       hovers = true,
@@ -74,30 +34,12 @@ lspconfig.tailwindcss.setup {
       -- },
     },
   },
-}
+})
 
--- C++ language server (Clangd)
-lspconfig.clangd.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
+vim.lsp.enable(servers)
 
--- Eslint language server
--- lspconfig.eslint.setup {
---   cmd = { "eslint" },
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
---   settings = {
---     workingDirectory = { mode = "auto" },
---   },
---   root_dir = lspconfig.util.root_pattern(".eslintrc", ".eslintrc.json", ".eslintrc.js", "package.json", ".git"),
--- }
-lspconfig.vtsls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
+vim.lsp.config("vtsls", {
+  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
   settings = {
     typescript = {
       format = {
@@ -117,9 +59,20 @@ lspconfig.vtsls.setup {
     },
     vtsls = {
       experimental = {
-        enableProjectDiagnostics = true, -- enables diagnostics across multiple TS projects
+        enableProjectDiagnostics = true,
       },
     },
   },
-  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-}
+})
+
+-- Eslint language server
+-- lspconfig.eslint.setup {
+--   cmd = { "eslint" },
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+--   settings = {
+--     workingDirectory = { mode = "auto" },
+--   },
+--   root_dir = lspconfig.util.root_pattern(".eslintrc", ".eslintrc.json", ".eslintrc.js", "package.json", ".git"),
+-- }
