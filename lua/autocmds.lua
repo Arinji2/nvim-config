5ab8abf82b7d06346ec937b666b6cd529a635c0a
Arinji2 <arinjaydhar205@gmail.com>
2025-06-23 09:04:19 +0530

    Add Vtsls code actions

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,6 +3,31 @@ require "nvchad.autocmds"
 local autocmd = vim.api.nvim_create_autocmd
 ---------------------------------------------- Markdown TOOLS ----------------------------------------------
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client or client.name ~= "vtsls" then
+      return
+    end
+
+    -- Buffer-local mapping (only for files where vtsls is active)
+    vim.keymap.set("n", "<leader>lo", function()
+      vim.lsp.buf.code_action {
+        ---@diagnostic disable-next-line
+        context = { only = { "source.organizeImports" }, diagnostics = {} },
+        apply = true,
+      }
+    end, { buffer = args.buf, desc = "LSP: Organize Imports" })
+
+    vim.keymap.set("n", "<leader>la", function()
+      vim.lsp.buf.code_action {
+        ---@diagnostic disable-next-line
+        context = { only = { "source.addMissingImports.ts" }, diagnostics = {} },
+        apply = true,
+      }
+    end, { buffer = args.buf, desc = "LSP: Add Missing Imports" })
+  end,
+})
 autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.mdx",
   command = "set filetype=markdown",
