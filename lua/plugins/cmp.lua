40bd0cc48be877e8a4ec19c5e886d781ed2d9ce2
Arinji2 <arinjaydhar205@gmail.com>
2025-07-22 23:08:58 +0530

    Add it back lol

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,3 +1,16 @@
 return {
   { import = "nvchad.blink.lazyspec" },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            preselect = true,
+            auto_insert = true,
+          },
+        },
+      },
+    },
+  },
 }
