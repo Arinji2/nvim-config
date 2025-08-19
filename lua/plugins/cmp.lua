1b649f1f6e8a077ad3655a5aeb82b8a5b1694c8e
Arinji2 <arinjaydhar205@gmail.com>
2025-08-19 21:15:21 +0530

    Remove unused blink extension

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,16 +1,3 @@
 return {
   { import = "nvchad.blink.lazyspec" },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      completion = {
-        list = {
-          selection = {
-            preselect = true,
-            auto_insert = true,
-          },
-        },
-      },
-    },
-  },
 }
