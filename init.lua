ee887027a058f24803d1e00eb8a210c5d85e86d1
Arinji2 <arinjaydhar205@gmail.com>
2026-04-01 00:57:47 +0530

    Remove oil setup from init since unused

--- init.lua
+++ init.lua
@@ -45,5 +45,3 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts.border = "single"
   return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
-
-require("oil").setup()
