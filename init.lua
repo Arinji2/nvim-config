e72fe92a7bdc33d469f64e4692cd317bc9a11e2f
Arinji2 <arinjaydhar205@gmail.com>
2026-04-01 01:00:20 +0530

    Ignore diagnostic error

--- init.lua
+++ init.lua
@@ -40,6 +40,8 @@ vim.lsp.inlay_hint.enable(true)
 require "snippets"
 
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+
+---@diagnostic disable-next-line: duplicate-set-field
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
   opts.border = "single"
